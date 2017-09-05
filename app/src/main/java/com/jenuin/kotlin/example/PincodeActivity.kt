5401572f7f771338b9a5a24c8c736e7c513b362e
Janu Prasad K <janu@hatio.co>
2017-09-05 12:21:48 +0530

    fixed progress bar

--- app/src/main/java/com/jenuin/kotlin/example/PincodeActivity.kt
+++ app/src/main/java/com/jenuin/kotlin/example/PincodeActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.os.StrictMode
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 
 
 class PincodeActivity : AppCompatActivity() {
 
     private lateinit var logger: TextView
+
+    private lateinit var progress: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +24,21 @@ class PincodeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pincode)
 
 
-//        val buttonSubmit: Button = findViewById(R.id.buttonSubmit) as Button
-//        val editTextInput = findViewById(R.id.editTextPinCode) as EditText
-//        logger = findViewById(R.id.app_log) as TextView
-//
-//        buttonSubmit.setOnClickListener({
-//            if (!isEmpty(editTextInput)) {
-//                val string = editTextInput.text.toString().trim()
-//                Log.v("TAG", string)
-//                getPincode(string)
-//            }
-//        })
-//
-//        appLogger("");
+        val buttonSubmit: Button = findViewById(R.id.buttonSubmit) as Button
+        val editTextInput = findViewById(R.id.editTextPinCode) as EditText
+        progress = findViewById(R.id.progress) as ProgressBar
+
+        logger = findViewById(R.id.app_log) as TextView
+
+        buttonSubmit.setOnClickListener({
+            if (!isEmpty(editTextInput)) {
+                val string = editTextInput.text.toString().trim()
+                Log.v("TAG", string)
+                getPincode(string)
+            }
+        })
+
+        appLogger("");
 
 
     }
@@ -41,12 +48,13 @@ class PincodeActivity : AppCompatActivity() {
     }
 
     private fun getPincode(pinCode: String) {
-
+        progress.visibility = View.VISIBLE
         val restAPI: RestAPI = RestAPI()
         var call = restAPI.getPinCode(pinCode)
         val response = call.execute()
         if (response.isSuccessful && response.body().status == "Success") {
             val place = response.body()
+            progress.visibility = View.INVISIBLE
             appLogger(buildString(pinCode, place.postOffice[0].name))
             Log.v("response", place.postOffice[0].name)
         }
